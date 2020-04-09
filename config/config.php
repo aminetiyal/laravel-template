f912c71e40883475e4e54c20a67519c40145d58c
Amine TIYAL <aminetiyal@gmail.com>
2020-04-09 11:44:50 +0100

    update assets

--- config/config.php
+++ config/config.php
@@ -79,39 +79,20 @@ return [
 
     // Assets to be loaded
     'assets' => [
-        // Fonts
-        'fonts' => [
-            'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700',
-        ],
-
         // Styles
         'styles' => [
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css',
-            'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
-            'https://adminlte.io/themes/v3/plugins/daterangepicker/daterangepicker.css',
-            'https://adminlte.io/themes/v3/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
-            'https://adminlte.io/themes/v3/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css',
-            'https://adminlte.io/themes/v3/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
-            'https://adminlte.io/themes/v3/plugins/select2/css/select2.min.css',
-            'https://adminlte.io/themes/v3/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css',
-            'https://adminlte.io/themes/v3/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css',
-            'https://adminlte.io/themes/v3/dist/css/adminlte.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/css/select2.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.0.5/daterangepicker.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/css/tempusdominus-bootstrap-4.min.css',
         ],
 
         // Scripts
         'scripts' => [
-            'https://adminlte.io/themes/v3/plugins/jquery/jquery.min.js',
-            'https://adminlte.io/themes/v3/plugins/bootstrap/js/bootstrap.bundle.min.js',
-            'https://adminlte.io/themes/v3/plugins/select2/js/select2.full.min.js',
-            'https://adminlte.io/themes/v3/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js',
-            'https://adminlte.io/themes/v3/plugins/moment/moment.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.0.5/moment.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.12/js/select2.min.js',
             'https://adminlte.io/themes/v3/plugins/inputmask/min/jquery.inputmask.bundle.min.js',
-            'https://adminlte.io/themes/v3/plugins/daterangepicker/daterangepicker.js',
-            'https://adminlte.io/themes/v3/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js',
-            'https://adminlte.io/themes/v3/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
-            'https://adminlte.io/themes/v3/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
-            'https://adminlte.io/themes/v3/dist/js/adminlte.min.js',
-            'https://adminlte.io/themes/v3/dist/js/demo.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.0.5/daterangepicker.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/js/tempusdominus-bootstrap-4.min.js',
         ]
     ]
 ];
--- resources/views/components/lte/styles.blade.php
+++ resources/views/components/lte/styles.blade.php
@@ -1,9 +1,9 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.3/css/adminlte.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
+
 @foreach($styles as $style)
     <link rel="stylesheet" href="{{$style}}">
-@endforeach
-
-@foreach($fonts as $font)
-    <link href="{{$font}}" rel="stylesheet">
 @endforeach
 
 @stack('styles')
