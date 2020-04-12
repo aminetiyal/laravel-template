89b12b6f5585012bd1cc56ad22221c32ad660a81
Amine TIYAL <aminetiyal@gmail.com>
2020-04-12 19:31:20 +0100

    fix ProfileController namespace

--- src/Http/Controllers/Auth/ProfileController.php
+++ src/Http/Controllers/Auth/ProfileController.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace Aminetiyal\LaravelTemplate\Http\Controllers\Auth;
-//namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
