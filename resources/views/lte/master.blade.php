aaf9f7ddca7555d0bd6c55d595ee64064a383740
Amine TIYAL <aminetiyal@gmail.com>
2020-04-13 02:21:09 +0100

    add title section and csrf meta

--- resources/views/lte/master.blade.php
+++ resources/views/lte/master.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{config('template.brand.name',config('app.name','AdminLte'))}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>@yield('title',config('template.brand.name',config('app.name','AdminLte')))</title>
     <x-lte-styles></x-lte-styles>
 </head>
 <body class="hold-transition @yield('bodyClasses')">
